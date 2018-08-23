f072873af0e826209971c7bc752852d3c6613481
肖龙 <xiaolong05@meituan.com>
2018-08-23 14:04:34 +0800

    更新

--- code/O(n)判断平衡二叉树.cpp
+++ code/O(n)判断平衡二叉树.cpp
@@ -27,7 +27,7 @@ int main() {
     //构造平衡树
     Node *root = new Node(1);
     Node *tree[20];
-    for(int i = 2;i <= 7;i ++) {
+    for(int i = 2;i <= 15;i ++) {
         tree[i] = new Node(i);
     }
     root -> left = tree[2];
@@ -36,7 +36,14 @@ int main() {
     tree[2] -> right = tree[5];
     tree[3] -> left = tree[6];
     tree[3] -> right = tree[7];
-
+    tree[4] -> left = tree[8];
+    tree[4] -> right = tree[9];
+    tree[5] -> left = tree[10];
+    tree[5] -> right = tree[11];
+    tree[6] -> left = tree[12];
+    tree[6] -> right = tree[13];
+    tree[7] -> left = tree[14];
+    tree[7] -> right = tree[15];
     //运行
     int ans = is_balance(root);
     if(ans == -1) {
